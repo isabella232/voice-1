ec303cecdcd18cf33b65d4731f06ed572665a72b
Adam Lerer <adam.lerer@gmail.com>
2010-04-21 18:23:22 +0000

    Added skipConfirmation option

--- src/org/odk/voice/constants/QuestionAttributes.java
+++ src/org/odk/voice/constants/QuestionAttributes.java
@@ -5,4 +5,5 @@ public class QuestionAttributes {
   public final static String AUDIO_SKIP_INSTRUCTIONS = "skipInstructions";
   public static final String SKIP_QUESTION_COUNT = "skipQuestionCount";
   public static final String REPEAT_QUESTION_OPTION = "repeatQuestionOption";
+  public static final String SKIP_CONFIRMATION = "skipConfirmation";
 }
